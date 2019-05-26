3f75e5e08beae501a273f5f77bd0c5b64a6b9a24
afnarqui <andresnaranjo@afnarqui.com>
2019-05-25 23:28:00 -0500

    add entity and models

---
+++ back-db/models/shoppingcart.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const setupDatabase = require('../lib/db')
+
+module.exports = function setupShoppingCartModel (config) {
+  const sequelize = setupDatabase(config)
+
+  return sequelize.define('shoppingcart', {
+    idProducto: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    }
+  })
+}
---
+++ back-db/models/user.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const setupDatabase = require('../lib/db')
+
+module.exports = function setupUserModel (config) {
+  const sequelize = setupDatabase(config)
+
+  return sequelize.define('user', {
+    uuid: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    state: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  })
+}
